f9d92dc46f469c9485ecf7ffa0f2fd5371de8e64
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2019-08-31 22:34:50 -0300

    Add procfile

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ const config = () => {
         case 'dev':
             return {
                 // Your connection string 
-                bd_string: '<sua URL de conexão>',
+                bd_string: 'mongodb+srv://admin:rbbcvgfhyt@ask-food-ownhb.mongodb.net/test?retryWrites=true&w=majority',
                 jwt_pass: 'sad2f31sad56f1asd5f6sa51f5sa6f5sa1fa8e9sadf21asd65',
                 jwt_expires_in: '1d',
             }
